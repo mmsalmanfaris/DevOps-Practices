b17583b6bdd84eb94399203c29943f5097aae66d
“mmsalmanfaris” <mmsalmanfaaris01@gmail.com>
2025-07-16 20:36:35 +0530

    basic ec2 instance terraform

---
+++ Terraform/AWS/ec2_instance/main.tf
@@ -0,0 +1,34 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.3.0"
+    }
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ec2_instance_terraform"
+  }
+}
