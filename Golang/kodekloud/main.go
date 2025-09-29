5981ddd533542d17f72f9081130267e50f048af4
“mmsalmanfaris” <mmsalmanfaaris01@gmail.com>
2025-09-29 23:47:38 +0530

    dynammodb

---
+++ Golang/kodekloud/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	// fmt.Println("Testing 123")
+
+	// var name string = "Salman Faris"
+
+	// var name string = "Salman Faris"
+	// var job string = "DevOps"
+
+	// fmt.Println("My name is ", name, "I'm a ", job, "Engineer")
+
+	// var grade int = 50
+
+	// fmt.Printf("My Az900 grade is %d", grade)
+
+
+	var name string = "Kode Kloud"
+	var age int = 10
+
+	fmt.Printf(" %v is %d old", name, age)
+}
---
+++ Terraform/kodekloud/dynamodb/main.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "cars" {
+  name = "cars"
+  hash_key = "VIN"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "VIN"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "car-items" {
+  table_name = aws_dynamodb_table.cars.name
+  hash_key = aws_dynamodb_table.cars.hash_key
+  item = jsonencode({
+    "Brand":{"S": "BMW"},
+    "Year":{"N": "2025"},
+    "VIN": {"S": "qwec234234df"}
+  })
+}
